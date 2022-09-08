cbbc01d992c1f093edb9766d915c2a67244b2620
Simon Mirovich <simonfrizen98@gmail.com>
2022-09-08 12:59:35 +0300

    Delete extra var

--- projects/test_tasks/HSE/task_2.py
+++ projects/test_tasks/HSE/task_2.py
@@ -11,8 +11,6 @@ df = read_data(
     "https://docs.google.com/spreadsheets/d/165sp-lWd1L4qWxggw25DJo_njOCvzdUjAd414NSE8co/edit#gid=1439079331"
 )
 
-num = 0
 for i in list(set(df.area)):
     plot_scatter(df, i)
     plt.savefig("images/" + i + ".png", dpi=300)
-    num += 1
